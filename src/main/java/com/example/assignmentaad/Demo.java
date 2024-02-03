da0cf7e862e4067e255233ba347471ddf4697448
DasunMadawa <dasunmadawa8864@gmail.com>
2024-02-03 11:08:24 +0530

    Initial commit

---
+++ src/main/java/com/example/assignmentaad/Demo.java
@@ -0,0 +1,22 @@
+package com.example.assignmentaad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/*")
+public class Demo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] segments = req.getPathInfo().split("/");
+        
+        System.out.println(segments[segments.length-1]);
+
+        System.out.println(req.getHeader("abc"));
+
+
+    }
+}
